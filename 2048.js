96b1505c575ad7563ea829ef89c1b9c03236ee95
HighTechMachineLearning <anthony.troussard@live.u-tad.com>
2026-09-21 19:24:36 +0200

    impl logica del juego

--- 2048.js
+++ 2048.js
@@ -1,71 +1,166 @@
-let empty_tiles = "";
-for (let i = 0; i < 16; i++) {
-    empty_tiles += '<div class="tile background-tile"></div>';
+const WIDTH = 4;
+const HEIGHT = 4;
+const TILE_COUNT = WIDTH * HEIGHT;
+const TRANSITION_DURATION = parseFloat(
+    getComputedStyle(document.documentElement)
+        .getPropertyValue("--transition-duration")
+        .trim()
+);
+
+const board = document.getElementById("board");
+const tiles = init_grid();
+
+setTimeout(() => spawn_random_tile(), 500);
+setTimeout(() => spawn_random_tile(), 600);
+
+document.addEventListener("keydown", (event) => {
+    let direction = null;
+    switch (event.key) {
+        case "ArrowUp": direction = DIRECTION_UP; break;
+        case "ArrowDown": direction = DIRECTION_DOWN; break;
+        case "ArrowLeft": direction = DIRECTION_LEFT; break;
+        case "ArrowRight": direction = DIRECTION_RIGHT; break;
+    }
+    if (direction !== null) move(direction);
+});
+
+const DIRECTION_UP = {
+    next: (index) => index - WIDTH,
+    reverse_iteration: false
+};
+const DIRECTION_DOWN = {
+    next: (index) => index + WIDTH,
+    reverse_iteration: true
+};
+const DIRECTION_LEFT = {
+    next: (index) => (index % WIDTH == 0) ? null : index - 1,
+    reverse_iteration: false
+};
+const DIRECTION_RIGHT = {
+    next: (index) => (index % WIDTH == WIDTH - 1) ? null : index + 1,
+    reverse_iteration: true
+};
+
+let timeout = null;
+function move(direction) {
+    if (timeout !== null) {
+        clearTimeout(timeout);
+        spawn_random_tile();
+        timeout = null;
+    }
+    for (const tile of tiles) if (tile !== null) tile.merged = false;
+    const dir = direction;
+    let something_moved = false;
+    for_each_tile_coord(dir.reverse_iteration, (x, y) => {
+        const src = y * WIDTH + x;
+        const tile = tiles[src];
+        if (tile === null) return;
+        let dst = src;
+        while (true) {
+            let i = dir.next(dst);
+            if (i === null || tiles[i] === undefined) break;
+            if (tiles[i] !== null) {
+                if (can_merge(src, i)) dst = i;
+                break;
+            }
+            dst = i;
+        }
+        if (src !== dst) {
+            if (can_merge(src, dst)) {
+                set_tile_value(tile, tile.value * 2);
+                tile.merged = true;
+            }
+            move_tile(src, dst);
+            something_moved = true;
+        }
+    });
+    if (something_moved) timeout = setTimeout(() => {
+        spawn_random_tile();
+        timeout = null
+    }, TRANSITION_DURATION);
 }
-document.getElementById("background-grid").innerHTML = empty_tiles;
 
-const tiles = [];
+function can_merge(src, dst) {
+    return !tiles[src]?.merged && !tiles[dst]?.merged
+        && tiles[src]?.value === tiles[dst]?.value;
+}
 
-for (let y = -1; y < 5; y++) {
-    for (let x = -1; x < 5; x++) {
-        let i = (x + y * 4);
-        setTimeout(() => tiles.push(spawn_tile_at(x, y, 2 ** (i + 1))), 1000 + 100 * i);
+function for_each_tile_coord(reverse_iteration, fn) {
+    if (reverse_iteration) {
+        for (let y = HEIGHT; y-- > 0;) {
+            for (let x = WIDTH; x-- > 0;) {
+                fn(x, y);
+            }
+        }
+    } else {
+        for (let y = 0; y < HEIGHT; y++) {
+            for (let x = 0; x < WIDTH; x++) {
+                fn(x, y);
+            }
+        }
     }
 }
 
-function spawn_tile_at(x, y, value) {
+function move_tile(src, dst) {
+    const tile = tiles[src];
+    if (tiles[dst] !== null) {
+        setTimeout((tile) => tile.element.remove(), TRANSITION_DURATION, tiles[dst]);
+    }
+    tiles[dst] = tile;
+    tiles[src] = null;
+    set_tile_css_position(tile, dst % WIDTH, Math.floor(dst / WIDTH));
+}
+
+function init_grid() {
+    let tiles = [];
+    let empty_tiles = "";
+    for (let i = 0; i < TILE_COUNT; i++) {
+        tiles.push(null);
+        empty_tiles += '<div class="tile background-tile"></div>';
+    }
+    document.getElementById("background-grid").innerHTML = empty_tiles;
+    return tiles;
+}
+
+function create_tile(x, y, value) {
     const str = value.toString();
     const element = document.createElement("div");
     element.classList.add("tile", "active-tile");
-    element.innerText = str;
-    element.style.fontSize = 6 * (5 / 6) ** Math.max(str.length - 3, 0) + "vmin";
-    element.style.backgroundColor = select_background_color(value);
-    document.getElementById("board").prepend(element);
+    board.prepend(element);
     const tile = {
-        x: x,
-        y: y,
         value: value,
         element: element,
+        merged: false,
     }
-    move_tile_to(tile, x, y);
+    set_tile_value(tile, value);
+    set_tile_css_position(tile, x, y);
     return tile;
 }
 
-function select_background_color(value) {
-    switch (value) {
-        case 2: return "aliceblue";
-        case 4: return "white";
-        case 8: return "white";
-        case 16: return "white";
-        case 32: return "white";
-        case 64: return "white";
-        case 128: return "white";
-        case 256: return "white";
-        case 512: return "white";
-        case 1024: return "white";
-        case 2048: return "gold";
-        case 4096: return "gold";
-        case 8192: return "gold";
-        case 16384: return "gold";
-        case 32768: return "gold";
-        case 65536: return "gold";
-        case 131072: return "gold";
-        default: return "pink";
-    }
-}
-
-function move_tile_to(tile, x, y) {
-    tile.x = x;
-    tile.y = y;
+function set_tile_css_position(tile, x, y) {
     tile.element.style.translate = (x * 18) + "vmin " + (y * 18) + "vmin";
 }
 
-document.addEventListener("click", () => {
-    for (const tile of tiles) {
-        move_tile_to(tile, random_int(0, 4), random_int(0, 4));
-    }
-});
+function set_tile_value(tile, value) {
+    const str = value.toString();
+    tile.value = value;
+    tile.element.innerText = str;
+    tile.element.style.fontSize = 6 * (5 / 6) ** Math.max(str.length - 3, 0) + "vmin";
+}
 
 function random_int(a, b) {
     return Math.floor(Math.random() * (b - a) + a);
+}
+
+function spawn_random_tile() {
+    const empty_tiles = [];
+    for (let i = 0; i < TILE_COUNT; i++) {
+        if (tiles[i] === null) empty_tiles.push(i);
+    }
+    if (empty_tiles.length == 0) return;
+    const index = empty_tiles[random_int(0, empty_tiles.length)];
+    const x = index % WIDTH;
+    const y = Math.floor(index / WIDTH);
+    const value = Math.random() < 0.1 ? 4 : 2;
+    tiles[index] = create_tile(x, y, value);
 }
